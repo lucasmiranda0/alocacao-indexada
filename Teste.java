d26262f070b70c544b2a73dd202d0064d03c45ea
Lucas Miranda <54516392+lucasmiranda0@users.noreply.github.com>
2022-03-27 20:47:43 -0300

    Update Teste.java

--- Teste.java
+++ Teste.java
@@ -15,30 +15,5 @@ public class Teste {
         ai.adicionarNoBuffer(filePath2);
         ai.adicionarNaParticao();
         ai.listaBlocoDeIndices.get(1).print();
-        /*
-        
-        AlocacaoIndexada ai = new AlocacaoIndexada();
-        ai.adicionarNoBuffer(filePath);
-        
-        byte[] particao = new byte[1000];
-        String filePath = "/home/lucas/Projects/so2/AlocacaoIndexada/teste.txt";
-        File file = new File(filePath);
-        byte[] bytes = Files.readAllBytes(file.toPath());
-        System.out.println(bytes.length);
-        
-        for (int i = 0; i < 10; i++) {
-            System.out.println(particao[i]);
-        }
-        
-        
-        byte[] myvar = "Any String you want".getBytes();
-        System.out.println(myvar.length);
-        
-        String s = new String(myvar);
-        String s2 = "aloha";
-        System.out.println(s);
-        byte[] myvar2 = (s + s2).getBytes();
-        System.out.println(myvar2.length);
-        */
-    }
+          }
 }
